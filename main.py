1a5c2013598c96580d6dd72b61b5ba31f76e242a
kelpikz <ajithaprasad96@gmail.com>
2021-03-26 21:01:00 +0530

    Feat : create models.py to add users to the db > Feat: Change database from postgres to sqlite Feat: Add .env to load the env variables

--- main.py
+++ main.py
@@ -1,84 +1,79 @@
 import discord
 import os
 import json
-import psycopg2
+from dotenv import load_dotenv
+from models import Database
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-curr = conn.cursor()
-
-curr.execute('''CREATE TABLE IF NOT EXISTS users ( 
-  user_id varchar ( 50 ) primary key, 
-  score int );''')
+load_dotenv(verbose=True)
 
 client = discord.Client()
 
-data_file = open('data.json')
+data_file = open("data.json")
 data = json.load(data_file)
 
-userdata_file = open('userdata.json')
+userdata_file = open("userdata.json")
 users = json.load(userdata_file)
 userdata_file.close()
 
+db = Database()
+
+
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
+
 
 @client.event
 async def on_message(message):
 
-    if message.author == client.user:
-        return
-    
-    else:
-        curr.execute("INSERT INTO users VALUES ('{0}', 0) ON CONFLICT DO NOTHING;".format(str(message.author)))
+    if message.content.startswith("$sup"):
+        print(message.author)
+        print(str(message.author))
+        res = db.find_user(str(message.author))
+        await message.channel.send(res)
+    # if message.content.startswith('$leaderboard'):
 
-    if message.content.startswith('$leaderboard'):
-        curr.execute("SELECT * from users;")
-        await message.channel.send(str(curr.fetchall()))
-
-    if message.content.startswith('$hello'):
+    if message.content.startswith("$hello"):
         intro_text = data["intro"].format(str(message.author))
         await message.channel.send(intro_text)
-        await message.channel.send(file=discord.File('thtest.png'))
+        await message.channel.send(file=discord.File("thtest.png"))
 
-    if message.content.startswith('$clue'):
-        
+    if message.content.startswith("$clue"):
+
         clueid = users.get(str(message.author), 0)
-        
+
         if clueid < 3:
-          await message.channel.send(data["clues"][clueid])
-        
+            await message.channel.send(data["clues"][clueid])
+
         else:
-          await message.channel.send(data["next_stage_toast"])
-    
-    if message.content.startswith('$ans'):
-        
-        ans_words = message.content.split(' ')
+            await message.channel.send(data["next_stage_toast"])
+
+    if message.content.startswith("$ans"):
+
+        ans_words = message.content.split(" ")
         answerid = users.get(str(message.author), 0)
 
         if answerid >= len(data["answers"]):
             await message.channel.send(data["next_stage_toast"])
-        
+
         elif len(ans_words) != 2 or ans_words[0] != "$ans":
             await message.channel.send(data["wrong_format_toast"])
-          
+
         else:
             if ans_words[1] == data["answers"][answerid]:
                 users[str(message.author)] = users.get(str(message.author), 0) + 1
                 with open("userdata.json", "w") as outfile:
                     json.dump(users, outfile)
                 await message.channel.send(data["correct_answer_toast"])
-            
+
                 if answerid < len(data["answers"]) - 1:
                     await message.channel.send(data["next_clue_toast"])
-            
+
                 else:
                     await message.channel.send(data["next_stage_toast"])
-          
+
             else:
                 await message.channel.send(data["wrong_answer_toast"])
 
-client.run(os.getenv('TOKEN')) 
+
+client.run(os.getenv("TOKEN"))
---
+++ models.py
@@ -0,0 +1,95 @@
+import sqlite3
+import datetime
+from sqlite3.dbapi2 import Error
+
+# def init_db():
+#   conn = sqlite3.connect("festemberHunt21")
+#   return con
+
+
+DB_NAME = "festemberHunt21.db"
+
+
+class Database:
+    """ Used to do crud operations on the db"""
+
+    def __init__(self):
+        self.conn, self.cursor = self.init_db()
+
+    def init_db(self):
+        try:
+            conn = sqlite3.connect(DB_NAME)
+            c = conn.cursor()
+            c.execute(
+                "SELECT name FROM sqlite_master WHERE type='table' AND name in ('users', 'score', 'clues');"
+            )
+            r = c.fetchall()
+            # print(r)
+            if len(r) == 3:
+                print("Tables already exist, setting up connection with db!")
+                return conn, c
+            print("Tables don't exist, creating them")
+            c.execute(
+                """CREATE TABLE IF NOT EXISTS users ( 
+				user_id integer PRIMARY KEY,
+				name text NOT NULL, 
+				time_joined datetime,
+				clue integer);"""
+            )
+            print("Created users table")
+            c.execute(
+                """CREATE TABLE IF NOT EXISTS clues (
+                clue_id integer PRIMARY KEY,
+                clue text,
+                clue_time datetime,
+                hint text,
+                hint_time datetime,
+                correct_toast text,
+                wrong_toast text
+            );"""
+            )
+            print("Created clues table")
+            c.execute(
+                """CREATE TABLE IF NOT EXISTS score (
+				score_id integer PRIMARY KEY,
+                user_id integer NOT NULL,
+				score integer,
+				FOREIGN KEY (user_id) REFERENCES users (id)
+				);"""
+            )
+            print("created score table")
+            print("Tables have been created, connecting to db")
+            conn.commit()
+            return conn, c
+        except Exception as e:
+            print("couldn't create db, {}".format(str(e)))
+            return "error", 400
+
+    def add_user(self, name):
+        """adds user to db and"""
+        try:
+            print("Inserting a person with the username, ", name)
+            time = datetime.datetime.now()
+            query = "INSERT INTO users(name, time_joined, clue) VALUES(?,?,?)"
+            self.cursor.execute(query, (name, time, 1))
+            self.conn.commit()
+            print("Created a user with the id:", self.cursor.lastrowid)
+            self.cursor.execute(
+                "SELECT * FROM users WHERE user_id=?", (self.cursor.lastrowid,)
+            )
+            result = self.cursor.fetchone()
+            print("created a user :", result)
+        except Exception as e:
+            print("couldn't create a row in the db, {}".format(str(e)))
+
+    def find_user(self, name):
+        """Returns the user item for the given username,
+        if such a user doesn't exist creates one"""
+        query = "SELECT * FROM users WHERE name=:name"
+        self.cursor.execute(query, {"name": name})
+        result = self.cursor.fetchone()
+        print("Got the result when trying to find a user:,", result)
+        if result == None:
+            print("Such a user doesn't exist")
+            return self.add_user(name)
+        return result
